1de8f31bc826b66062c330ceb22bc01cbe80f3cb
Padma <prlama@deerhold.org>
2020-04-20 14:17:29 +0545

    build commit

---
+++ packages/react-client/build/precache-manifest.4a94cd47200b8815ddf5f64adbb2519a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "20c7e67301341cb2e0c04d5684296a93",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ba250d4692c942338684",
+    "url": "/static/js/2.c2b248e6.chunk.js"
+  },
+  {
+    "revision": "4d7ef388ded4c46d8f17e43a588896c3",
+    "url": "/static/js/2.c2b248e6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "02912bcb32c33b361cdf",
+    "url": "/static/js/main.8692b5f7.chunk.js"
+  },
+  {
+    "revision": "702554416a81a1e3a894",
+    "url": "/static/js/runtime-main.784c586e.js"
+  }
+]);
